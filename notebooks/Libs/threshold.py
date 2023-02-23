cbd4f38e87c5ec615b7053a78c1c71114454cf0d
falric05 <falco.riccardo@outlook.it>
2023-02-23 11:06:03 +0000

    minor changes

--- notebooks/Libs/threshold.py
+++ notebooks/Libs/threshold.py
@@ -3,8 +3,12 @@ from itertools import product
 
 def get_labels_physics(grid, params):
     thr = np.ones(grid.shape)
-    for s, m in product(params['sigma'], params['mu']):
-        thr[:, params['sigma'].index(s), :, params['mu'].index(m), :, :] *= m * (1 + s)
+    for r in range(params['run']):
+        for s, t, d, m in product(params['sigma'], params['theta'], 
+                                    params['delta'], params['mu']):
+            si = params['sigma'].index(s)
+            mi = params['mu'].index(m)
+    thr[r, si, :, mi, :, :] *= m * (1 + s)
     return grid > thr
 
 def get_labels_KDE(grid, params):
