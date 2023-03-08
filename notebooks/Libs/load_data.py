acb411ea8e9586ce3ad295695e0044821e65bb30
DavideFemia <femiadavide97@gmail.com>
2023-03-09 00:14:48 +0100

    added ClassificationDataLoader and a first version of a CNN model

--- notebooks/Libs/load_data.py
+++ notebooks/Libs/load_data.py
@@ -5,6 +5,175 @@ from pathlib import Path
 from tqdm import tqdm
 
 from Libs.config import data_folder
+from Libs.threshold import get_labels_physic, get_labels_KDE, get_labels_quantile, get_labels_quantile_on_run
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def get_dataset_split(grid_X, grid_y, idx, test_size = 0.2, val_size=0.2, window_size = 10, overlap_size = 0, label_treshold=1,
+                      split_on_run=False, shuffle_run=False, shuffle_window=False, get_validation=False, random_state=42):
+    def build_df(X_configuration, y_configuration, window_size=window_size, overlap_size=overlap_size, label_treshold = label_treshold):
+        stride = window_size - overlap_size
+        num_windows = (X_configuration.shape[-1]-window_size)//stride + 1
+
+        windows = np.zeros((X_configuration.shape[0]*(num_windows-1),window_size))
+        windows_label = np.zeros((y_configuration.shape[0]*(num_windows-1),window_size), dtype='bool')
+
+
+        for i in range(X_configuration.shape[0]):
+            tmp_windows = np.array([X_configuration[i,j:j+window_size] for j in range(0,stride*num_windows,stride)])
+            tmp_windows_labels = np.array([y_configuration[i,j:j+window_size] for j in range(0,stride*num_windows,stride)])
+            windows[i*(num_windows-1):(i+1)*(num_windows-1),:] = tmp_windows[:-1,:]
+            windows_label[i*(num_windows-1):(i+1)*(num_windows-1),:] = tmp_windows_labels[1:,:]
+
+        windows_label = np.sum(windows_label, axis=-1)
+        windows_label[windows_label<label_treshold] = 0
+        windows_label[windows_label>=label_treshold] = 1
+        
+        df = pd.DataFrame(windows, columns=[f't_{i}' for i in range(windows.shape[-1])])
+        y_df = pd.DataFrame({'future_flare':windows_label})
+        df = pd.concat([df, y_df], axis=1)
+
+        return df
+
+    if len(grid_X.shape) == 2:
+        grid_X = np.copy(grid_X).reshape(grid_X.shape[0],1,1,1,1,grid_X.shape[1])
+        grid_y = np.copy(grid_y).reshape(grid_y.shape[0],1,1,1,1,grid_y.shape[1])
+    
+    if split_on_run:
+        if get_validation:
+            train_size = 1-test_size-val_size
+            run_val_index = int(train_size * grid_X.shape[0])
+            run_test_index = int((train_size+val_size) * grid_X.shape[0])
+        else:
+            train_size = 1-test_size
+            run_test_index = int(train_size * grid_X.shape[0])
+        
+        
+        # Train split
+        if shuffle_run:
+            np.random.seed(random_state)
+            run_perm = np.random.permutation(grid_X.shape[0])
+            if get_validation:
+                X_train_configuration = grid_X[run_perm[:run_val_index], idx[0], idx[1], idx[2], idx[3], :]
+                y_train_configuration = grid_y[run_perm[:run_val_index], idx[0], idx[1], idx[2], idx[3], :]
+                X_val_configuration = grid_X[run_perm[run_val_index:run_test_index], idx[0], idx[1], idx[2], idx[3], :]
+                y_val_configuration = grid_y[run_perm[run_val_index:run_test_index], idx[0], idx[1], idx[2], idx[3], :]
+            else:
+                X_train_configuration = grid_X[run_perm[:run_test_index], idx[0], idx[1], idx[2], idx[3], :]
+                y_train_configuration = grid_y[run_perm[:run_test_index], idx[0], idx[1], idx[2], idx[3], :]
+            X_test_configuration = grid_X[run_perm[run_test_index:], idx[0], idx[1], idx[2], idx[3], :]
+            y_test_configuration = grid_y[run_perm[run_test_index:], idx[0], idx[1], idx[2], idx[3], :]
+        else:
+            if get_validation:
+                X_train_configuration = grid_X[:run_val_index, idx[0], idx[1], idx[2], idx[3], :]
+                y_train_configuration = grid_y[:run_val_index, idx[0], idx[1], idx[2], idx[3], :]
+                X_val_configuration = grid_X[run_val_index:run_test_index, idx[0], idx[1], idx[2], idx[3], :]
+                y_val_configuration = grid_y[run_val_index:run_test_index, idx[0], idx[1], idx[2], idx[3], :]
+            else:
+                X_train_configuration = grid_X[:run_test_index, idx[0], idx[1], idx[2], idx[3], :]
+                y_train_configuration = grid_y[:run_test_index, idx[0], idx[1], idx[2], idx[3], :]
+            X_test_configuration = grid_X[run_test_index:, idx[0], idx[1], idx[2], idx[3], :]
+            y_test_configuration = grid_y[run_test_index:, idx[0], idx[1], idx[2], idx[3], :]
+        
+        # here df_train and df_test doesn't contain windows that share runs
+        df_train = build_df(X_train_configuration, y_train_configuration)
+        if get_validation:
+            df_val = build_df(X_val_configuration, y_val_configuration)
+        df_test = build_df(X_test_configuration, y_test_configuration)
+    else:
+        X_configuration = grid_X[:, idx[0], idx[1], idx[2], idx[3], :]
+        y_configuration = grid_y[:, idx[0], idx[1], idx[2], idx[3], :]
+        df = build_df(X_configuration, y_configuration)
+        if get_validation:
+            train_size = 1-test_size-val_size
+            window_val_index = int(train_size * df.shape[0])
+            window_test_index = int((train_size+val_size) * df.shape[0])
+        else:
+            train_size = 1-test_size
+            window_test_index = int(train_size * df.shape[0])
+        if get_validation:
+            df_train = df.iloc[:window_val_index,:]
+            df_val = df.iloc[window_val_index:window_test_index,:]
+            df_test = df.iloc[window_test_index:,:]
+        else:
+            df_train, df_test = df.iloc[:window_test_index,:], df.iloc[window_test_index:,:]
+    
+    if shuffle_window:
+        df_train = df_train.sample(frac=1, random_state=random_state)
+    
+    if get_validation:
+        return df_train, df_val, df_test
+    else:
+        return df_train, df_test
+
+class ClassificationDataLoader():
+    params = {}
+    time_series = None
+    labels = None
+    
+    def __closed_form_method(self, theta, mu, sigma, delta_t, x0=1, N=1000):
+        X = np.zeros(N)
+        X[0] = x0
+        W = np.random.normal(0, 1, size=N)
+        W[0] = 0
+        for i in range(N-1):
+            X[i+1] = X[i] + theta*(mu - X[i])*delta_t + sigma*W[i]*X[i]*np.sqrt(delta_t) + 0.5*(sigma**2)*X[i]*delta_t*(W[i]**2 - 1)
+        return X
+    
+    def __init__(self, run=1000, N=1000, s=0.5, t=0.01, d=0.2, m=1):
+        self.params['run'] = run
+        self.params['sigma'] = [s]
+        self.params['theta'] = [t]
+        self.params['mu'] = [m]
+        self.params['delta'] = [d]
+        self.params['N'] = N
+        
+    def load_data(self, labelling_method=get_labels_physic, verbose=True):
+        
+        s = self.params['sigma'][0]
+        t = self.params['theta'][0]
+        m = self.params['mu'][0]
+        d = self.params['delta'][0]
+        
+        self.time_series = np.zeros((self.params['run'], self.params['N']))
+        
+        filename = 'classification_data'
+        if verbose:
+                print('Loading Data')
+        if not os.path.exists(os.path.join(data_folder, filename+'.npy')):
+            generator = range(self.params['run'])
+            if verbose:
+                generator = tqdm(generator)
+            for r in generator:
+                self.time_series[r,:] = self.__closed_form_method(t, m, s, d)
+            np.save(os.path.join(data_folder, filename), self.time_series, allow_pickle=True)
+        self.time_series = np.load(os.path.join(data_folder, filename+'.npy'), allow_pickle=True)
+            
+        
+        if verbose:
+            print('Loading Labels')
+        adapter_grid = np.copy(self.time_series).reshape(self.time_series.shape[0],1,1,1,1,self.time_series.shape[1])
+        self.labels = labelling_method(adapter_grid, self.params, classification=True)[:,0,0,0,0,:]
+        
+        if verbose:
+            print('Labels Loaded')
+            
+        return np.copy(self.time_series), np.copy(self.labels)
+        
+    
+    def get_params(self):
+        """
+        Return params dictionary
+        """
+        return self.params
+    
+    def get_standard_indexes(self):
+        """
+        Return standard values indeces
+        """
+        return (0,0,0,0)
+        
 
 class DataLoader():
     params = {
--- notebooks/Libs/threshold.py
+++ notebooks/Libs/threshold.py
@@ -5,7 +5,7 @@ import os
 
 from Libs.config import data_folder
 
-def get_labels_physic(grid, params, alpha=1):
+def get_labels_physic(grid, params, alpha=1, classification=False):
     """
     Compute the threshold by considering the parameters that generated the current light curve
     as `thr = μ + α*σ*μ`
@@ -14,7 +14,12 @@ def get_labels_physic(grid, params, alpha=1):
     * `params`: params dictionary
     * `alpha`: weight coefficient of stimated stochastic component (α), by default is equal to 1
     """
-    if not os.path.exists(os.path.join(data_folder, 'labels_physic.npy')):
+    if classification:
+        filename = 'classification_labels_physic'
+    else:
+        filename = 'labels_physic'
+            
+    if not os.path.exists(os.path.join(data_folder, filename+'.npy')):
         thr = np.ones(grid.shape)
         for r in range(params['run']):
             for s, m in product(params['sigma'], params['mu']):
@@ -23,12 +28,12 @@ def get_labels_physic(grid, params, alpha=1):
                 thr[r, si, :, mi, :, :] *= m * (1 + alpha*s)
         pred = grid >= thr
         # store labels
-        np.save(os.path.join(data_folder, 'labels_physic'), pred, allow_pickle=True)
+        np.save(os.path.join(data_folder, filename), pred, allow_pickle=True)
     # load labels
-    pred = np.load(os.path.join(data_folder, 'labels_physic'+'.npy'), allow_pickle=True)
+    pred = np.load(os.path.join(data_folder, filename+'.npy'), allow_pickle=True)
     return pred
 
-def get_labels_KDE(grid, params, quantile=0.99):
+def get_labels_KDE(grid, params, quantile=0.99, classification=False):
     """
     Estimate the threshold following the KDE anomaly detection approach
     ## Params
@@ -36,7 +41,12 @@ def get_labels_KDE(grid, params, quantile=0.99):
     * `params`: params dictionary
     * `alpha`: weight coefficient of stimated stochastic component (α), by default is equal to 1
     """
-    if not os.path.exists(os.path.join(data_folder, 'labels_anomaly_detection.npy')):
+    if classification:
+        filename = 'classification_labels_anomaly_detection'
+    else:
+        filename = 'labels_anomaly_detection'
+    
+    if not os.path.exists(os.path.join(data_folder, filename+'.npy')):
         pred = np.ones(grid.shape, dtype=bool)
 
         for s, t, d, m in product(params['sigma'], params['theta'], params['delta'], params['mu']):
@@ -69,45 +79,63 @@ def get_labels_KDE(grid, params, quantile=0.99):
             thr = np.mean(l)
             pred[:, si, ti, mi, di] = signal >= thr
         # store labels
-        np.save(os.path.join(data_folder, 'labels_anomaly_detection'), pred, allow_pickle=True)
+        np.save(os.path.join(data_folder, filename), pred, allow_pickle=True)
     # load labels
-    pred = np.load(os.path.join(data_folder, 'labels_anomaly_detection'+'.npy'), allow_pickle=True)
+    pred = np.load(os.path.join(data_folder, filename+'.npy'), allow_pickle=True)
     return pred
 
-def get_labels_quantile(grid, params, percentile=0.8):
-    labels = np.copy(grid)
-    for s, t, d, m in product(params['sigma'], params['theta'],params['delta'], params['mu']):
-        si = params['sigma'].index(s)
-        ti = params['theta'].index(t)
-        di = params['delta'].index(d)
-        mi = params['mu'].index(m)
-        # stima del percentile
-        values = np.sort(grid[:,si,ti,mi,di,:].flatten())
-        # normalized_values = values/values[-1]
-        treshold = values[int(percentile*params['run']*params['N'])] # 0.8 * 30 * 1000
-        view = labels[:,si,ti,mi,di,:]
-        view[view>treshold] = 1
-        view[view<=treshold] = 0
-    return labels.astype('bool')
-
-def get_labels_quantile_on_run(grid, params, percentile=0.8):
-    labels = np.copy(grid)
-    for ri in range(params['run']):
+def get_labels_quantile(grid, params, percentile=0.8, classification=False):
+    if classification:
+        filename = 'classification_labels_quantile'
+    else:
+        filename = 'labels_quantile'
+    
+    if not os.path.exists(os.path.join(data_folder, filename+'.npy')):
+        labels = np.copy(grid)
         for s, t, d, m in product(params['sigma'], params['theta'],params['delta'], params['mu']):
             si = params['sigma'].index(s)
             ti = params['theta'].index(t)
             di = params['delta'].index(d)
             mi = params['mu'].index(m)
             # stima del percentile
-            values = np.sort(grid[ri,si,ti,mi,di,:])
-            normalized_values = values/values[-1]
-            treshold = normalized_values[int(percentile*params['N'])]
-            view = labels[ri,si,ti,mi,di,:]
+            values = np.sort(grid[:,si,ti,mi,di,:].flatten())
+            # normalized_values = values/values[-1]
+            treshold = values[int(percentile*params['run']*params['N'])] # 0.8 * 30 * 1000
+            view = labels[:,si,ti,mi,di,:]
             view[view>treshold] = 1
             view[view<=treshold] = 0
-    return labels.astype('bool')
+        result = labels.astype('bool')
+        np.save(os.path.join(data_folder, filename), pred, allow_pickle=True)
+    result = np.load(os.path.join(data_folder, filename+'.npy'), allow_pickle=True)
+    
+    return result
 
-def get_labels_mean_std(grid, params):
+def get_labels_quantile_on_run(grid, params, percentile=0.8, classification=False):
+    if classification:
+        filename = 'classification_labels_quantile_on_run'
+    else:
+        filename = 'labels_quantile_on_run'
+    
+    if not os.path.exists(os.path.join(data_folder, filename+'.npy')):
+        labels = np.copy(grid)
+        for ri in range(params['run']):
+            for s, t, d, m in product(params['sigma'], params['theta'],params['delta'], params['mu']):
+                si = params['sigma'].index(s)
+                ti = params['theta'].index(t)
+                di = params['delta'].index(d)
+                mi = params['mu'].index(m)
+                # stima del percentile
+                values = np.sort(grid[ri,si,ti,mi,di,:])
+                normalized_values = values/values[-1]
+                treshold = normalized_values[int(percentile*params['N'])]
+                view = labels[ri,si,ti,mi,di,:]
+                view[view>treshold] = 1
+                view[view<=treshold] = 0
+        result = labels.astype('bool')
+    result = np.load(os.path.join(data_folder, filename+'.npy'), allow_pickle=True)
+    return result
+
+def get_labels_mean_std(grid, params, classification=False):
     # l = 2*theta/((sigma)**2)
     # mean = l * mu / (l-1)
     # std = np.sqrt( l*(mu**2) / (l-2) )
